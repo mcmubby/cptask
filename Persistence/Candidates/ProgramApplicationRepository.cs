aed6195f0324248c8a93416cef22fb7df9a96b35
mcmubby <12859241+mcmubby@users.noreply.github.com>
2024-05-14 08:42:14 +0100

    Added persistence and repositories

---
+++ Persistence/Candidates/ProgramApplicationRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Candidates;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Candidates
+{
+    public class ProgramApplicationRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly Container _applicationsContainer;
+
+        public ProgramApplicationRepository(CosmosClient cosmosClient, IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            var dbName = _configuration["DbSettings:DbName"];
+            var containerName = "Applications";
+            _applicationsContainer = cosmosClient.GetContainer(dbName, containerName);
+        }
+
+        public async Task<ProgramApplication> CreateApplicationAsync(ProgramApplication application)
+        {
+            var response = await _applicationsContainer.CreateItemAsync(application);
+            return response.Resource;
+        }
+    }
+}
---
+++ Persistence/Employers/ProgramOfferingRepository.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Employers;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Employers
+{
+    public class ProgramOfferingRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly Container _offeringsContainer;
+
+        public ProgramOfferingRepository(CosmosClient cosmosClient, IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            var dbName = _configuration["DbSettings:DbName"];
+            var containerName = "Offerings";
+            _offeringsContainer = cosmosClient.GetContainer(dbName, containerName);
+        }
+
+        public async Task<ProgramOffering> CreateProgramAsync(ProgramOffering program)
+        {
+            var response = await _offeringsContainer.CreateItemAsync(program);
+            return response.Resource;
+        }
+
+        public async Task<ProgramOffering> GetProgramByIdAsync(string programId)
+        {
+            var query = _offeringsContainer.GetItemLinqQueryable<ProgramOffering>()
+            .Where(o => o.Id == programId)
+            .Take(1)
+            .ToQueryDefinition();
+
+            var response = await _offeringsContainer.GetItemQueryIterator<ProgramOffering>(query).ReadNextAsync();
+            return response.FirstOrDefault();
+        }
+
+        public async Task<ProgramOffering> UpdateProgramAsync(ProgramOffering program)
+        {
+            var response = await _offeringsContainer.UpsertItemAsync(program);
+            return response.Resource;
+        }
+    }
+}
