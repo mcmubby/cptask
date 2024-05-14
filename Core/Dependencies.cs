a4a6768b32478d52301dc0fc05c4a636e5d58a70
mcmubby <12859241+mcmubby@users.noreply.github.com>
2024-05-14 10:50:48 +0100

    Added services and dependecies

---
+++ Core/Dependencies.cs
@@ -0,0 +1,17 @@
+﻿using Core.Candidates.Interfaces;
+using Core.Candidates.Services;
+using Core.Employers.Interfaces;
+using Core.Employers.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core
+{
+    public static class Dependencies
+    {
+        public static void AddCoreServices(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationService, ApplicationService>();
+            services.AddScoped<IOfferingService, OfferingService>();
+        }
+    }
+}
---
+++ Persistence/Dependencies.cs
@@ -0,0 +1,33 @@
+﻿using Core.Persistence.Interfaces;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Candidates;
+using Persistence.Employers;
+
+namespace Persistence
+{
+    public static class Dependencies
+    {
+        public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton((provider) =>
+            {
+                var uri = configuration["DbSettings:Uri"];
+                var dbName = configuration["DbSettings:DbName"];
+                var primaryKey = configuration["DbSettings:PrimaryKey"];
+
+                var cosmosClientOptions = new CosmosClientOptions { ApplicationName = dbName };
+
+                var cosmosClient = new CosmosClient(uri, primaryKey, cosmosClientOptions);
+                //cosmosClient.CreateDatabaseIfNotExistsAsync(uri).Wait();
+
+                return cosmosClient;
+            });
+
+            services.AddScoped<IProgramApplicationRepository, ProgramApplicationRepository>();
+            services.AddScoped<IProgramOfferingRepository, ProgramOfferingRepository>();
+
+        }
+    }
+}
